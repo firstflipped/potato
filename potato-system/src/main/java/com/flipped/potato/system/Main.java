c8e0a20daa84858f5f72d757575d95462d388427
flipped <firstflipped@163.com>
2023-10-26 17:15:13 +0800

    feat: 添加所需依赖

---
+++ potato-system/src/main/java/com/flipped/potato/system/Main.java
@@ -0,0 +1,14 @@
+package com.flipped.potato.system;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author <a href="#">flipped</a>
+ * @version v1.0
+ * @since 2023-08-20 20:14:40
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
